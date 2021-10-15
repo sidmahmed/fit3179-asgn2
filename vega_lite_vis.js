d8f7267e0346a1153356753bed07f971991cb48e
ahmedbonyeamin <ahmed.bonyeamin@gmail.com>
2021-10-15 14:15:02 +1100

    added unemployment.csv

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,6 +1,7 @@
 var visa_year = "visa-year.vl.json";
 var industry_year = "industry-year.vl.json";
 var top_industry = "top-industry.vl.json";
+var occupation_rank_employer = "occupation-rank-employer.vl.json";
 
 vegaEmbed("#visa_year", visa_year).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
@@ -10,9 +11,13 @@ vegaEmbed("#industry_year", industry_year).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
    }).catch(console.error);
 
-   vegaEmbed("#top_industry", top_industry).then(function(result) {
+vegaEmbed("#top_industry", top_industry).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#occupation_rank_employer", occupation_rank_employer).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-   }).catch(console.error);
+    }).catch(console.error);
 
 
 
