45b4baad62da19a3e6a0521b1e079b3c0ed5bc2c
ahmedbonyeamin <ahmed.bonyeamin@gmail.com>
2021-10-15 15:01:22 +1100

    added state-resident.csv

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,6 +2,7 @@ var visa_year = "visa-year.vl.json";
 var industry_year = "industry-year.vl.json";
 var top_industry = "top-industry.vl.json";
 var occupation_rank_employer = "occupation-rank-employer.vl.json";
+var unemployment = "unemployment.vl.json";
 
 vegaEmbed("#visa_year", visa_year).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
@@ -18,6 +19,12 @@ vegaEmbed("#top_industry", top_industry).then(function(result) {
 vegaEmbed("#occupation_rank_employer", occupation_rank_employer).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
+
+    vegaEmbed("#unemployment", unemployment).then(function(result) {
+        // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+        }).catch(console.error);
+
+
 
 
 
