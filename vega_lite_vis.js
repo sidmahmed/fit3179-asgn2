67dfa7f48b04842965a6bf5a65e7e84853bb0bc8
ahmedbonyeamin <ahmed.bonyeamin@gmail.com>
2021-10-19 00:05:31 +1100

    added grid

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,7 +3,7 @@ var industry_year = "industry-year.vl.json";
 var top_industry = "top-industry.vl.json";
 var occupation_rank_employer = "occupation-rank-employer.vl.json";
 var unemployment = "unemployment.vl.json";
-var map = "map.json";
+var map = "map.vl.json";
 
 vegaEmbed("#visa_year", visa_year).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
